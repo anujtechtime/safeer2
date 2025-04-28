acaf144b62b13a3f3247d719ea52a148eb61add1
anujtechtime <anuj@techtime.me>
2025-04-28 11:13:38 +0530

    added

--- safeer_purchase/models/models.py
+++ safeer_purchase/models/models.py
@@ -65,25 +65,25 @@ class SalerefOrder(models.Model):
             self.order_line = new_order_lines            
 
 
-class AccountMoveLineLi(models.Model):
-    _inherit = 'account.move.line'
+# class AccountMoveLineLi(models.Model):
+#     _inherit = 'account.move.line'
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        iqd_currency = self.env.ref('base.IQD', raise_if_not_found=False)
-        if not iqd_currency:
-            raise ValidationError("IQD currency must exist in the system.")
+#     @api.model_create_multi
+#     def create(self, vals_list):
+#         iqd_currency = self.env.ref('base.IQD', raise_if_not_found=False)
+#         if not iqd_currency:
+#             raise ValidationError("IQD currency must exist in the system.")
 
-        company_currency = self.env.company.currency_id
-        for vals in vals_list:
-            # Only apply if currency is not set and company currency is USD
-            if company_currency.name == 'USD':
-                # Get the exchange rate for the journal entry date
-                move_date = vals.get('date') or fields.Date.context_today(self)
-                rate = iqd_currency.with_context(date=move_date).rate or 0.0
-                if rate > 0.0:
-                    usd_to_iqd = 1 / rate
-                    amount = vals.get('debit') or vals.get('credit') or 0.0
-                    vals['currency_id'] = iqd_currency.id
-                    vals['amount_currency'] = round(amount * usd_to_iqd, 3)
-        return super(AccountMoveLineLi, self).create(vals_list)         
+#         company_currency = self.env.company.currency_id
+#         for vals in vals_list:
+#             # Only apply if currency is not set and company currency is USD
+#             if company_currency.name == 'USD':
+#                 # Get the exchange rate for the journal entry date
+#                 move_date = vals.get('date') or fields.Date.context_today(self)
+#                 rate = iqd_currency.with_context(date=move_date).rate or 0.0
+#                 if rate > 0.0:
+#                     usd_to_iqd = 1 / rate
+#                     amount = vals.get('debit') or vals.get('credit') or 0.0
+#                     vals['currency_id'] = iqd_currency.id
+#                     vals['amount_currency'] = round(amount * usd_to_iqd, 3)
+#         return super(AccountMoveLineLi, self).create(vals_list)         
